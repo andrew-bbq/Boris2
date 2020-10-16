28014c7656e0a0404d960329bc707e2a967236db
andrew-bbq <awbennett@wisc.edu>
2020-10-16 13:18:24 -0500

    Add generic code, probably need to tune

---
+++ generate_tweet.py
@@ -0,0 +1,29 @@
+import gpt_2_simple as gpt2
+
+# the guide said this works on a 1080ti and I'm running a 2070 super so...
+# here we go
+model_name = "124M"
+
+# Download the model if it is not present
+if not os.path.isdir(os.path.join("models", model_name)):
+    print(f"Downloading {model_name} model...")
+    gpt2.download_gpt2(model_name=model_name)
+
+# Start a Tensorflow session to pass to gpt2_simple
+sess = gpt2.start_tf_sess()
+
+# Define the number of steps we want our model to take we want this to be such that
+# we only pass over the data set 1-2 times to avoid overfitting.
+num_steps = 60
+
+# This is the path to the text file we want to use for training.
+text_path = "data.txt"
+
+# Pass in the session and the
+gpt2.finetune(sess,
+              text_path,
+              model_name=model_name,
+              steps=num_steps
+             )
+
+gpt2.generate(sess)
